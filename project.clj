f9696aee552eccd920bdb9b2716e27d3bca8c1ae
Leo Lou <louyuhong@gmail.com>
2018-04-12 09:15:36 +0900

    Add aliases

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :main dactyl-keyboard.dactyl
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :plugins [[lein-auto "0.1.3"]
+            [lein-exec "0.3.7"]]
+  :aliases {"generate" ["exec" "-p" "src/dactyl_keyboard/dactyl.clj"]}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [unicode-math "0.2.0"]
                  [scad-clj "0.4.0"]])
