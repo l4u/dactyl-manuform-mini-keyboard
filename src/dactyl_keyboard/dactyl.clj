4456b31e118342db14fbf9464a70447260812ca2
Leo Lou <louyuhong@gmail.com>
2018-04-21 10:11:34 +0900

    Clean up unused teensy code

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -619,40 +619,6 @@
   ; rectangular trrs holder
    (->> (apply cube usb-holder-size) (translate [(first usb-holder-position) (+ (/ wall-thickness 2) (second usb-holder-position) (/ usb-holder-thickness 2)) (+ (/ (last usb-holder-size) 2) usb-holder-thickness)]))))
 
-(def teensy-width 20)
-(def teensy-height 12)
-(def teensy-length 33)
-(def teensy2-length 53)
-(def teensy-pcb-thickness 2)
-(def teensy-holder-width  (+ 7 teensy-pcb-thickness))
-(def teensy-holder-height (+ 6 teensy-width))
-(def teensy-offset-height 5)
-(def teensy-holder-top-length 18)
-(def teensy-top-xy (key-position 0 (- centerrow 1) (wall-locate3 -1 0)))
-(def teensy-bot-xy (key-position 0 (+ centerrow 1) (wall-locate3 -1 0)))
-(def teensy-holder-length (- (second teensy-top-xy) (second teensy-bot-xy)))
-(def teensy-holder-offset (/ teensy-holder-length -2))
-(def teensy-holder-top-offset (- (/ teensy-holder-top-length 2) teensy-holder-length))
-
-(def teensy-holder
-  (->>
-   (union
-    (->> (cube 3 teensy-holder-length (+ 6 teensy-width))
-         (translate [1.5 teensy-holder-offset 0]))
-    (->> (cube teensy-pcb-thickness teensy-holder-length 3)
-         (translate [(+ (/ teensy-pcb-thickness 2) 3) teensy-holder-offset (- -1.5 (/ teensy-width 2))]))
-    (->> (cube 4 teensy-holder-length 4)
-         (translate [(+ teensy-pcb-thickness 5) teensy-holder-offset (-  -1 (/ teensy-width 2))]))
-    (->> (cube teensy-pcb-thickness teensy-holder-top-length 3)
-         (translate [(+ (/ teensy-pcb-thickness 2) 3) teensy-holder-top-offset (+ 1.5 (/ teensy-width 2))]))
-    (->> (cube 4 teensy-holder-top-length 4)
-         (translate [(+ teensy-pcb-thickness 5) teensy-holder-top-offset (+ 1 (/ teensy-width 2))])))
-   (translate [(- teensy-holder-width) 0 0])
-   (translate [-1.4 0 0])
-   (translate [(first teensy-top-xy)
-               (- (second teensy-top-xy) 1)
-               (/ (+ 6 teensy-width) 2)])))
-
 (defn screw-insert-shape [bottom-radius top-radius height]
   (union
    (->> (binding [*fn* 30]
@@ -749,10 +715,7 @@
         rj9-holder
         usb-holder-hole
                     ; usb-holder-hole
-                    ; ; teensy-holder-hole
                     ;             screw-insert-outers
-                    ;             teensy-screw-insert-holes
-                    ;             teensy-screw-insert-outers
                     ;             usb-cutout
                     ;             rj9-space
                                 ; wire-posts
@@ -763,7 +726,6 @@
        (cut
         (translate [0 0 -0.1]
                    (difference (union case-walls
-                                      teensy-holder
                                       pro-micro-holder
                                           ; rj9-holder
                                       screw-insert-outers)
