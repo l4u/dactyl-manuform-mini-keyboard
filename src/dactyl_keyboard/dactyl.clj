cfd580673ac3d352de86da3fae703a4cb4105bc7
Leo Lou <louyuhong@gmail.com>
2018-05-20 11:49:21 +0900

    Change pro micro position

--- src/dactyl_keyboard/dactyl.clj
+++ src/dactyl_keyboard/dactyl.clj
@@ -566,7 +566,7 @@
 
 (def usb-jack (translate (map + usb-holder-position [0 10 1.5]) (cube 8 20 3)))
 
-(def pro-micro-position (map + (key-position 0 2 (wall-locate3 -1 0)) [-6 0 -20]))
+(def pro-micro-position (map + (key-position 0 2 (wall-locate3 -1 0)) [-6.2 0 -20]))
 (def pro-micro-space-size [4 18 12]) ; z has no wall;
 (def pro-micro-wall-thickness 2)
 (def pro-micro-holder-size [(+ pro-micro-wall-thickness (first pro-micro-space-size)) (+ pro-micro-wall-thickness (second pro-micro-space-size)) (last pro-micro-space-size)])
